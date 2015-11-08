69cfdfedd4f9312b42a9912677a0f55664cb41e7
ryanlennon <ryanlennon07@gmail.com>
2015-11-07 20:32:07 -0600

    Solved common element

--- common_element.rb
+++ common_element.rb
@@ -1,5 +1,11 @@
 # Find the common element between two arrays.
-
+def common_element(input_array1, input_array2)
+  input_array1.each do |index|
+    if input_array2.include?(index)
+      puts index
+    end
+  end
+end
 
 # Driver code
 p common_element([2, 3, 2], [1, 9, 3]) # should be 3
