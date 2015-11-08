b8340d23f38ed6efc699e3a488db848335646033
ryanlennon <ryanlennon07@gmail.com>
2015-11-07 19:57:56 -0600

    Solved palindrome

--- palindrome.rb
+++ palindrome.rb
@@ -1,5 +1,12 @@
 # Determine if a string is a palindrome (case insensitive)
-
+def palindrome?(word)
+  new_word = word.downcase
+  if new_word.reverse == new_word
+     true
+   else 
+     false
+   end
+ end
 
 # Driver code
 p palindrome?("kayak") # should be true
